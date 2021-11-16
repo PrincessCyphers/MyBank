9c4bd0e8fe1558f140e1adf3954f718d370bd0b2
PrincessMav <princess-iza@hotmail.co.uk>
2021-11-16 18:04:13 +0000

    My bank classes; main and acocunt class

---
+++ src/Account.java
@@ -0,0 +1,12 @@
+/*
+Account class w
+ */
+public class Account {
+    int balance;
+    int recentTransaction;
+    String firstName;
+    String lastName;
+    String customerID = "MB0001";
+
+
+}
---
+++ src/MyBank.java
@@ -0,0 +1,16 @@
+/*
+A bank app that allows:
+- creation of new account holder
+- balance enquiry
+- money withdrawal
+- money deposition
+- view most recent transaction
+- interest calculation
+- Exit
+ */
+
+public class MyBank {
+    public static void main(String[] args) {
+
+    }
+}
